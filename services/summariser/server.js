4ae1b3dc63224ff007d1b5e50fe0744a4b6c917d
Peter <pwhelanstudio@gmail.com>
2025-05-10 12:51:50 +0100

    Creating Summariser Server file

---
+++ services/summariser/server.js
@@ -0,0 +1,8 @@
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const path = require('path');
+
+const PROTO_PATH = path.join(__dirname, '../../proto/summariser_service.proto');
+const packageDefinition = protoLoader.loadSync(PROTO_PATH);
+const summariserProto = grpc.loadPackageDefinition(packageDefinition).summariser;
+
